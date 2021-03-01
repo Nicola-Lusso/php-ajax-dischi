0a07363e460b2830b6b47e301196a2edc716d297
Nicola <nicolusso@gmail.com>
2021-03-01 16:14:54 +0100

    inizio

---
+++ index_php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="dist/app.css">
+    <title>Spotify-PHP</title>
+  </head>
+  <body>
+
+  </body>
+</html>
